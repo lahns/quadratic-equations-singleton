1257196dd4a104b85c0c3d19a2f3f4fb663630c3
async <81372963+lahns@users.noreply.github.com>
2022-03-21 23:58:38 +0100

    add files
    init

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works!");
+                s1.setNumber(5, 9, 2);
+                Console.WriteLine(s1.Delta());
+                Console.WriteLine("Square root: "+s1.SqrtDelta());
+            }
+            else
+            {
+                Console.WriteLine("Holy potatoes!");
+            }
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private Singleton() { }
+
+        private static Singleton _instance;
+        double a, b, c = 0;
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+        public void setNumber(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b; 
+            this.c = c; 
+        }
+        public double Delta()
+        {
+            double delta;
+            delta = this.b*this.b - 4 * this.a * this.c;
+            return delta;
+        }
+
+        public double SqrtDelta()
+        {
+            return Math.Sqrt(Delta());
+        }
+    }
+}
